44872c3dad2bce7d88efea3aa2a94392ce15aaf6
Christian Theune <ct@flyingcircus.io>
2024-01-03 10:18:01 +0100

    add simple cache busting

--- src/menage2/routes.py
+++ src/menage2/routes.py
@@ -1,5 +1,13 @@
+import time
+
+from pyramid.static import QueryStringConstantCacheBuster
+
+
 def includeme(config):
     config.add_static_view("static", "static", cache_max_age=3600)
+    config.add_cache_buster(
+        "static", QueryStringConstantCacheBuster(str(int(time.time())))
+    )
 
     config.add_route("suggest_ingredient", "/ingredient/suggest")
 
